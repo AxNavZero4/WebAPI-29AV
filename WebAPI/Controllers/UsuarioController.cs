77b3d4ea1ad0c87415e13110740a2a1edd121d4c
Axel Muñoz <Axel Muoz@DESKTOP-CRO0PQ4>
2025-05-25 23:30:15 -0600

    Agregar archivos de proyecto.

---
+++ WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Domain.DTO;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services.IServices;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioServices _usuarioServices;
+
+        public UsuarioController(IUsuarioServices usuarioServices)
+        {
+            _usuarioServices = usuarioServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var response = await _usuarioServices.GetAll(); 
+
+            return Ok(response);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var response = await _usuarioServices.GetbyId(id);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UsuarioRequest request)
+        {
+            var respose = await _usuarioServices.Create(request);
+            return Ok(respose);
+        }
+        
+       
+    }
+}
---
+++ WebAPI/Services/IServices/IUsuarioServices.cs
@@ -0,0 +1,16 @@
+﻿using Domain.DTO;
+using Domain.Entities;
+
+namespace WebAPI.Services.IServices
+{
+    public interface IUsuarioServices
+    {
+        public Task<Response<List<Usuario>>> GetAll();
+        public Task<Response<Usuario>> GetbyId(int id);
+        public Task<Response<Usuario>> Create(UsuarioRequest request);
+
+
+
+
+    }
+}
